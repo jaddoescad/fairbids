68c42c2ad1b218450422041e1b38ccbacc09d216
Jad Slim <jadslimm@gmail.com>
2024-04-18 10:07:46 -0400

    dd

--- services/getJobById.ts
+++ services/getJobById.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@/utils/supabase/client";
 
-export async function getJobById(jobId) {
+export async function getJobById(jobId: string) {
   const supabase = createClient();
 
   const { data, error } = await supabase
