3733252a7e1b0b39cc2bfe6ea8046c63add86670
Jad Slim <jadslimm@gmail.com>
2024-04-23 16:31:03 -0400

    Add user authentication and display user name in NavBar

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -8,15 +8,23 @@ import HeaderWrapper from "@/components/wrappers/HeaderWrapper";
 import MainWrapper from "@/components/wrappers/MainWrapper";
 import { LocationBar, SearchBar } from "@/components/LocationBar";
 import NavBar from "@/components/NavBar";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { getUserDisplayName } from "@/utils/getName";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: { user }} = await supabase.auth.getUser();
+  const name = getUserDisplayName(user);
+
   return (
     <Box w={"full"} h={"full"} display={"flex"} flexDirection={"column"}>
-      <NavBar />
+      <NavBar user={user} name={name}/>
       <MainWrapper>{children}</MainWrapper>
     </Box>
   );
